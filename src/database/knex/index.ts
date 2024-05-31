7179dda4740c4cde74a49ce194437a199cc56586
tarcisio <tarcisiomateus98@gmail.com>
2024-05-30 21:31:52 -0300

    made create-users migration

--- src/database/knex/index.ts
+++ src/database/knex/index.ts
@@ -1,4 +1,4 @@
-import { knex as setUpKnex, Knex } from 'knex'
+import setUpKnex, { Knex } from 'knex'
 
 export const config: Knex.Config = {
   client: 'sqlite',
@@ -12,4 +12,4 @@ export const config: Knex.Config = {
   },
 }
 
-export const knex = setUpKnex({})
+export const knex = setUpKnex(config)
